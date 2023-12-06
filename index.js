b77a3a40a615028f0216cc4a9dbeb0bb56df6841
HJ <uhyeonjin0619@gmail.com>
2023-12-06 15:03:08 +0900

    connect db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,22 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// db연결하기
+const db = new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database:"world",
+  password:"1234",
+  port: "5432",
+});
+db.connect();
+
 app.get("/", async (req, res) => {
+  const result = await db.query("SELECT country_code FROM visited_country");
+  //DB와 연결, table name 꼭 확인
+
+
+
   //Write your code here.
 });
 
